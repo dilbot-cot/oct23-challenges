5e99f222b67d8319935b87432ba2845fc6768f32
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-04-13 12:20:30 +1000

    fix: corrected spelling

--- c1_rotateString.js
+++ c1_rotateString.js
@@ -6,7 +6,7 @@
 var rotateString = function(s, goal) {
     // had to include this as it failed on a submission where s='aa' and goal = 'a'
     if (goal.length !== s.length) return false; 
-    // define variable 'let' as a concatination of the s string twice
+    // define variable 'test' as a concatination of the s string twice
     let test = s+s;
     // using the built in function 'includes' check if the goal string is included in the concatenated string
     return test.includes(goal);
